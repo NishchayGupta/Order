2eefa7b1410f8892d3bf5e5489aa7eba6372c386
1894832 <1894832@E21026.CEGEPGIM.LCL>
2019-02-09 10:17:55 -0500

    Added Customer class

---
+++ src/javaapplication11/Customer.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication11;
+
+/**
+ *
+ * @author 1894832
+ */
+public class Customer {
+    String name;
+    String location;
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public String getLoaction()
+    {
+        return location;
+    }
+    
+    public void setName(String newName)
+    {
+        name = newName;
+    }
+    
+    public void setLocation(String newLocation)
+    {
+        location = newLocation;
+    }
+    
+    public void sendOrder()
+    {
+        
+    }
+}
